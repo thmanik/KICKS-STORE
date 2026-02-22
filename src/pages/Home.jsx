6c8a74b0583b40c281298ceb879e93e42c288ca7
thmanik <thmanik1241@gmail.com>
2026-02-22 11:42:52 +0600

    feat: implement LatestArrivals section with responsive grid and ProductCardPrimary

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
+import LatestArrivals from '../components/home/LatestArrivals';
 
 // import NewDrops from './HomeSections/NewDrops';
 // import CategoriesSection from './HomeSections/CategoriesSection';
@@ -10,9 +11,8 @@ const Home = () => {
     <div className="flex flex-col gap-y-16 md:gap-y-24 mb-20 overflow-x-hidden">
       
       <Hero />
-      {/* <NewDrops />
-      <CategoriesSection />
-      <ReviewsSection /> */}
+      <LatestArrivals/>
+      
     </div>
   );
 };
